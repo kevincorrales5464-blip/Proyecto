a66af0deb5fd66550fca433dc27109ac8016fc0d
kevincorrales5464-blip <kevincorrales5464@politecnicomayor.edu.co>
2026-05-20 20:27:00 -0500

    actualizacion index

--- index.php
+++ index.php
@@ -1,27 +1,47 @@
-<?php
-session_start();
-include("conexion.php");
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="stileInicio.css">
+</head>
+<body>
+    <div class="container">
+        <h2> Iniciar Sesiòn</h2>
+        
+        <form method="POST" action="login.php">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            <input type="text" name="usuario" placeholder="Usuario" required>
+            <input type="password" name="password" placeholder="Contraseña" required>
+            <button type="submit" name="login">Ingresar</button>
+        </form>
 
-    $usuario = $_POST['usuario'];
-    $password = $_POST['password'];
+        <a href="registro.php">¿No tienes una cuenta? Regístrate aquí</a>
 
-    $sql = "SELECT * FROM usuarios WHERE usuario='$usuario'";
-    $res = mysqli_query($conexion, $sql);
+        <?php
+        if (isset($_SESSION['usuario'])) {
+            include("conexion.php");
 
-    if ($fila = mysqli_fetch_assoc($res)) {
+            $usuario = $_POST['usuario'];
+            $password = $_POST['password'];
 
-        if (password_verify($password, $fila['password'])) {
-            $_SESSION['usuario'] = $usuario;
-            header("Location: home.php");
-            exit();
-        } else {
-            echo "<p>Contraseña incorrecta</p>";
-        }
+            $sql = "SELECT * FROM  usuarios WHERE usuario=`$usuario'";
+            $res = mysqli_query($conexion, $sql);
 
-    } else {
-        echo "<p>Usuario no existe</p>";
-    }
-}
-?>
+            if ($fila = mysqli_fetch_assoc($res)) {
+                if (password_verify($password, $fila['password'])) {
+                    $_SESSION['usuario'] = $usuario;
+                    header("Location: home.php");
+                }
+                } else {
+                    echo "<p>Contraseña incorrecta</p>";
+                }
+            }else {
+                echo "<p>Usuario no existente</p>";
+            }
+        <?php
+    </div>
+</body>
+</html>
